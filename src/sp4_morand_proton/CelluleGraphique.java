655a56eb64b1bf9bfae2911a0d8f993e1d84170f
proto <proto@LAPTOP-TND8NS36.education.lakanal.epf.fr>
2022-12-04 01:03:33 +0100

    création et avancé de l'interface et de la Cellule graphique

---
+++ src/sp4_morand_proton/CelluleGraphique.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_morand_proton;
+import javax.swing.JButton;
+/**
+ *
+ * @author proto
+ */
+public class CelluleGraphique extends jButton {
+    Cellule CelluleAssociee;
+    //probleme avec le dossier image
+    
+    
+    
+    public CelluleGraphique (Cellule uneCellule){
+        CelluleAssociee= uneCellule;
+    }
+    
+}
